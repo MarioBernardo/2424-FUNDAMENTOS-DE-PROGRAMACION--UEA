ecdf0b1a8130b685abd176d6a2ddadc1db979b7d
BUMBLEEBEE <md.bernardoc@uea.edu.ec>
2024-08-31 00:49:07 -0500

    Deber semana 11

--- Opraciones con Arreglos/Ejercicio 2.py
+++ Opraciones con Arreglos/Ejercicio 2.py
@@ -1,7 +1,23 @@
 # Ejercicio 2
 # Matriz de 3 x 3
 matriz = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [9, 2, 5],
+    [4, 1, 8],
+    [7, 6, 3]
 ]
+
+print(matriz)
+
+#Ordenamiento de la  fila buble_sort
+def buble_sort(fila):
+    #algoritmo buble_sort
+    n = len(fila)
+    for i in range(n):
+        for j in range(n-1, i, -1):
+            if fila[j] > fila[j-1]:
+                fila[j], fila[j-1] = fila[j-1], fila[j]
+                print(fila)
+print("Matriz original")
+print(matriz)
+buble_sort(matriz[2])
+print(matriz)
